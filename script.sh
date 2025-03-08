9e276ba7e86d64266c75a5dbcc567270ec7d5488
fengqyv5 <119144021+fengqyv5@users.noreply.github.com>
2025-03-08 11:35:51 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf -O ChinaList.conf -q
 sed -i -e 's/server=/[/g' -e 's/114.114.114.114/]https:\/\/dns.alidns.com\/dns-query/g' ChinaList.conf
-echo "tls://8.8.4.4" >> ChinaList.conf
-echo "tls://dns.google" >> ChinaList.conf
+echo "sdns://AQIAAAAAAAAAETk0LjE0MC4xNC4xNDo1NDQzINErR_JS3PLCu_iZEIbq95zkSV2LFsigxDIuUso_OQhzIjIuZG5zY3J5cHQuZGVmYXVsdC5uczEuYWRndWFyZC5jb20" >> ChinaList.conf
+echo "sdns://AQIAAAAAAAAAGFsyYTEwOjUwYzA6OmFkMTpmZl06NTQ0MyDRK0fyUtzywrv4mRCG6vec5EldixbIoMQyLlLKPzkIcyIyLmRuc2NyeXB0LmRlZmF1bHQubnMxLmFkZ3VhcmQuY29t" >> ChinaList.conf
+echo "sdns://AQAAAAAAAAAADjIwOC42Ny4yMjAuMjIwILc1EUAgbyJdPivYItf9aR6hwzzI1maNDL4Ev6vKQ_t5GzIuZG5zY3J5cHQtY2VydC5vcGVuZG5zLmNvbQ" >> ChinaList.conf
+echo "sdns://AQAAAAAAAAAAD1syNjIwOjA6Y2NjOjoyXSC3NRFAIG8iXT4r2CLX_WkeocM8yNZmjQy-BL-rykP7eRsyLmRuc2NyeXB0LWNlcnQub3BlbmRucy5jb20" >> ChinaList.conf
 echo >> ChinaList.conf
 mv ChinaList.conf ChinaList.txt
